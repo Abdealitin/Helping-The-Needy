c1746cd215f55839f5ffd9c0a19c18a355e49eb4
Abdealitin <abdealitin.at@gmail.com>
2020-09-25 17:40:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/helptheneedy/MainActivity.java
@@ -0,0 +1,89 @@
+package com.example.helptheneedy;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private FirebaseUser mUser;
+    private TextView emailField;
+    private TextView password;
+    private Button btnsign;
+    private Button btnlogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                mUser = firebaseAuth.getCurrentUser();
+                if(mUser != null){
+                    Toast.makeText(MainActivity.this, "Signed In", Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(MainActivity.this, "Not Signed In", Toast.LENGTH_LONG).show();
+                }
+            }
+        };
+
+        emailField =(TextView) findViewById(R.id.editTextTextEmailAddress);
+        password =(TextView) findViewById(R.id.editTextTextPassword);
+        btnsign =(Button)findViewById(R.id.signpage);
+        btnsign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openActivity2();
+            }
+        });
+        btnlogin =(Button)findViewById(R.id.button);
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(emailField.getText().toString()) && !TextUtils.isEmpty(password.getText().toString())){
+
+                    String email = emailField.getText().toString();
+                    String pwd = password.getText().toString();
+                    login(email, pwd)
+                    openActivity1();
+                }else{
+
+                }
+                openActivity1();
+            }
+        });
+        public void login(String, String){
+
+        }
+    }
+
+    public void openActivity2(){
+        Intent intent = new Intent(this,usersign.class);
+        startActivity(intent);
+    }
+    public void openActivity1(){
+        Intent intent = new Intent(this,homePage.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+}
---
+++ app/src/main/java/com/example/helptheneedy/usersign.java
@@ -0,0 +1,71 @@
+package com.example.helptheneedy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+//import android.content.Intent;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+//import java.util.ArrayList;
+//import java.util.List;
+
+public class usersign extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+    private Spinner spin;
+    private Button signUp;
+    private Button login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_usersign);
+
+        spin = (Spinner) findViewById(R.id.spin);
+        signUp = (Button) findViewById(R.id.signupButton);
+        login  = (Button) findViewById(R.id.loginButton);
+
+       /* List<String> cities = new ArrayList<String>();
+        cities.add("Bhopal");
+        cities.add("Indore");
+        cities.add("Ujjain");*/
+
+        spin.setOnItemSelectedListener(this);
+
+        //ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cities);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.cities, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spin.setAdapter(adapter);
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(usersign.this, "Registered Succeessfully!", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(usersign.this, homePage.class);
+                startActivity(intent);
+            }
+        });
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(usersign.this,MainActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> parent) {
+
+    }
+}
